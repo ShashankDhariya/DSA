e71b7449db4630986a9372fe917348e007f759b6
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-09-07 10:57:32 +0530

    P

---
+++ Patterns/Pattern5.cpp
@@ -0,0 +1,23 @@
+/*
+    Pattern: 
+            1
+            22
+            333
+            4444
+            55555
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++)
+            cout<<"*";
+        cout<<endl;
+    }
+    return 0;
+}
