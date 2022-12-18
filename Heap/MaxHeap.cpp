77ea8873fdd93d7c822fdf4a769f26039d2458d9
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-12-19 00:36:43 +0530

    HP

---
+++ Heap/MaxHeap.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void heapify(int a[], int n, int i){
+    int largest = i;
+    int l = 2*i;
+    int r = 2*i + 1;
+
+    if(l < n && a[l] > a[largest])
+        largest = l;
+
+    if(r < n && a[r] > a[largest])
+        largest = r;
+
+    if(i != largest){
+        swap(a[largest], a[i]);
+        heapify(a, n, largest);
+    }
+}
+
+void buildHeap(int a[], int n){
+    for(int i=n/2;i>0;i--)
+        heapify(a, n, i);
+}
+
+int main(){
+    int arr[] = {0, 20, 10, 40, 5, 50, 30};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout<<"Before: ";
+    for(int i=1;i<size;i++)
+        cout<<arr[i]<<" ";
+    buildHeap(arr, size);
+
+    cout<<endl<<"After: ";
+    for(int i=1;i<size;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
